c87ed59892c18a19121c36fecb8105f089f30f38
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2020-10-28 16:55:00 -0300

    final-part-one

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
 
 import Header from './components/Header';
 
 function App() {
 
-  const [ projects, setProjects ] = useState(['Desenvolvimento de app', 'Front-end web']);  
+  const [ projects, setProjects ] = useState([]);  
 
-  function hundleAddProject() {
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+
+  async function hundleAddProject() {
     
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
 
-    console.log(projects);
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Luís Júnior Rodrigues Zembruski"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+
   }
 
   return (
@@ -18,7 +35,7 @@ function App() {
       <Header title="Projects" />
 
       <ul>
-        {projects.map(project => <li  key={project}>{project}</li>)}
+        {projects.map(project => <li  key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={hundleAddProject}>Adicionar Projetos</button>
