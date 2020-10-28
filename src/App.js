a812aeec0565eb415635faa41665d7f68cdaa433
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2020-10-28 12:22:12 -0300

    almoço 28-10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+
+  const [ projects, setProjects ] = useState(['Desenvolvimento de app', 'Front-end web']);  
+
+  function hundleAddProject() {
+    
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => <li  key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={hundleAddProject}>Adicionar Projetos</button>
     </>
   );
 }
